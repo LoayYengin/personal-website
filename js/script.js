b44184ab96158033f5ac021a7a83351545a6d670
LoayYengin <68306108+LoayYengin@users.noreply.github.com>
2021-05-21 20:22:09 -0500

    added hamburger menu animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger menu');
+
+    if (btnHamburger.classList.contains('menu-open')) {
+        btnHamburger.classList.remove('menu-open');
+    } else {
+        btnHamburger.classList.add('menu-open');
+    }
+
+});
