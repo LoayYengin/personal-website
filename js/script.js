10ca8d6e95cc1950e884af00a145f40210dd8d10
LoayYengin <68306108+LoayYengin@users.noreply.github.com>
2021-05-23 21:35:23 -0500

    added mobile overlay animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
-btnHamburger.addEventListener('click', function(){
+btnHamburger.addEventListener('click', function () {
     console.log('open hamburger menu');
 
-    if (btnHamburger.classList.contains('menu-open')) {
-        btnHamburger.classList.remove('menu-open');
-    } else {
-        btnHamburger.classList.add('menu-open');
+    if (header.classList.contains('menu-open')) { // close hamburger
+        header.classList.remove('menu-open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+    } else { // open hamburger
+        header.classList.add('menu-open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
     }
 
 });
