db17f6cba4a9dcee3802bdd1fca988761df13782
LoayYengin <68306108+LoayYengin@users.noreply.github.com>
2021-06-05 13:06:52 -0500

    changed closeNav() overflow from hidden to scroll

--- js/script.js
+++ js/script.js
@@ -28,10 +28,11 @@
 /* Open */
 function openNav() {
     document.getElementById("myNav").style.display = "block";
-    document.getElementById("home-content").style.overflow = "hidden";
+    document.querySelector("body").style.overflow = "hidden";
 }
 
 /* Close */
 function closeNav() {
     document.getElementById("myNav").style.display = "none";
+    document.querySelector("body").style.overflow = "scroll";
 }
