b36af703c4f01acf2fda8f28c69ead970f8eea06
LoayYengin <68306108+LoayYengin@users.noreply.github.com>
2021-06-05 12:50:59 -0500

    redesigned header and hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const body = document.querySelector('body');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
-const fadeElements = document.querySelectorAll('.has-fade');
+// const btnHamburger = document.querySelector('#btnHamburger');
+// const body = document.querySelector('body');
+// const header = document.querySelector('.header');
+// const overlay = document.querySelector('.overlay');
+// const fadeElements = document.querySelectorAll('.has-fade');
+//
+// btnHamburger.addEventListener('click', function () {
+//     console.log('open hamburger menu');
+//
+//     if (header.classList.contains('menu-open')) { // close hamburger
+//         body.classList.remove('no-scroll');
+//         header.classList.remove('menu-open');
+//         fadeElements.forEach(function(element) {
+//             element.classList.remove('fade-in');
+//             element.classList.add('fade-out');
+//         });
+//     } else { // open hamburger
+//         body.classList.add('no-scroll');
+//         header.classList.add('menu-open');
+//         fadeElements.forEach(function(element) {
+//             element.classList.add('fade-in');
+//             element.classList.remove('fade-out');
+//         });
+//     }
+//
+// });
 
-btnHamburger.addEventListener('click', function () {
-    console.log('open hamburger menu');
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+    document.getElementById("home-content").style.overflow = "hidden";
+}
 
-    if (header.classList.contains('menu-open')) { // close hamburger
-        body.classList.remove('no-scroll');
-        header.classList.remove('menu-open');
-        fadeElements.forEach(function(element) {
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
-        });
-    } else { // open hamburger
-        body.classList.add('no-scroll');
-        header.classList.add('menu-open');
-        fadeElements.forEach(function(element) {
-            element.classList.add('fade-in');
-            element.classList.remove('fade-out');
-        });
-    }
-
-});
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
